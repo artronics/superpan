39d3d6e700428b972331406e7598cdebbd239ce2
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-08-18 18:42:06 +0200

    add bin superpan and clean up cargo.toml

---
+++ superpan/src/main.rs
@@ -0,0 +1,9 @@
+extern crate ieee8021504;
+
+use ieee8021504::phy::frame::{Frame, FrameCtrl};
+
+fn main() {
+    let f = Frame::new(vec!(0,0,0,0,0x7A,5,6,0,123,100,100)).unwrap();
+
+    println!("Hello, world! {:?}", f.ack_requested());
+}
